da94533f12cb477bb488137c843702eae2c4d312
kingsimba <kingsimba@mapbar.com>
2020-04-28 12:51:39 +0800

    update

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -4,8 +4,9 @@ import { sum } from './test';
 describe('sum()', () => {
     it('should work', () => {
         // BUG: 
-        // This test failed as expected. There is an error icon on line 5, also as expected.
-        // But these is NO error message along line 9
+        // This test failed as expected. There is an error icon on line 5, 
+        // also as expected.
+        // But these is NO error message along line 10
         expect(sum(1, 2)).equals(4);
     });
 });
--- test/test.spec.js
+++ test/test.spec.js
@@ -5,8 +5,9 @@ const test_1 = require("./test");
 describe('sum()', () => {
     it('should work', () => {
         // BUG: 
-        // This test failed as expected. There is an error icon on line 5, also as expected.
-        // But these is NO error message along line 9
+        // This test failed as expected. There is an error icon on line 5, 
+        // also as expected.
+        // But these is NO error message along line 10
         chai_1.expect(test_1.sum(1, 2)).equals(4);
     });
 });
